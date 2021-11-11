9e0ce2662326b27f757b4983f0af02122081a033
aristides1000 <aristides1000@gmail.com>
2021-11-11 19:39:36 -0400

    Change a class for a function

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,23 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Greeting from './Greeting';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={(
-              <div>
-                <h1>Hello World</h1>
-              </div>
-          )}
-          />
-          <Route path="/greeting" element={<Greeting />} />
-        </Routes>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route
+        exact
+        path="/"
+        element={(
+          <div>
+            <h1>Hello World</h1>
+          </div>
+      )}
+      />
+      <Route path="/greeting" element={<Greeting />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
